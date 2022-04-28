5a5d1a991b687532779b8d794115befd0bd7025f
JhordP <j.horda.ny@hotmail.com>
2022-04-28 14:48:13 -0400

    Comentarios de SobrecargaMetodos 2da parte

--- src/operaciones/MainOperaciones.java
+++ src/operaciones/MainOperaciones.java
@@ -2,7 +2,7 @@ package operaciones;
 
 public class MainOperaciones {
     public static void main(String[] args) {
-        System.out.println(Operaciones.sumar(2, 3));
-        System.out.println(Operaciones.sumar(2.5, 1.5));
+        System.out.println(Operaciones.sumar(2, 3)); //Llama al sumar int porque recibe parámetros enteros
+        System.out.println(Operaciones.sumar(2.5, 1.5)); //Llama al sumar int porque recibe parámetros enteros
     }
 }
--- src/operaciones/Operaciones.java
+++ src/operaciones/Operaciones.java
@@ -2,10 +2,13 @@ package operaciones;
 
 //Sobrecarga de Metodos
 public class Operaciones {
+
+    //Sumar int
     public static int sumar(int a, int b){
         return a+b;
     }
 
+    //Sumar double. Se puede llamar igual pero debe cambiar el tipo de dato que recibe.
     public static double sumar(double a, double b) {
         return a+b;
     }
