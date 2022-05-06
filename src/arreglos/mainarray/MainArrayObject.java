7e9ba9e252586e28623e2005574971b982d18f9d
JhordP <j.horda.ny@hotmail.com>
2022-05-06 18:05:12 -0400

    Notas arrays.

--- src/arreglos/mainarray/MainArrayObject.java
+++ src/arreglos/mainarray/MainArrayObject.java
@@ -15,11 +15,12 @@ public class MainArrayObject{
         personas[1] = new Persona("Juan");
         personas[2] = new Persona("Karla");
         
+        System.out.println("\nUtilizando objetos o instancias en un array.");
         System.out.println("persona 0: "+personas[0]);
         System.out.println("persona 1: "+personas[1]);
-        System.out.println("persona 2: "+personas[2].getNombre());
+        System.out.println("persona 2: "+personas[2].getNombre()); //Utilizando el metodo get, que es lo mismo solo para demostrar que es simplemente un objeto con un index.
 
-        
+        System.out.println("\nUtilizando declaracion por definicion.");
         //Arreglos definidos en la declaracion
         String fruta[] = {"Pera","Manzana","Fresa","Naranja"};
         for (int i = 0; i < fruta.length; i++) {
