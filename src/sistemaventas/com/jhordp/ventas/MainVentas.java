62c17fe4ad3365f85e4f2b3822ca59ff988b236c
JhordP <j.horda.ny@hotmail.com>
2022-05-09 21:08:24 -0400

    Inicio: Sistema de Ventas en Java Console.

---
+++ src/sistemaventas/com/jhordp/ventas/MainVentas.java
@@ -0,0 +1,11 @@
+package sistemaventas.com.jhordp.ventas;
+
+public class MainVentas {
+
+    //Atributos de la clase
+    private int idProducto;
+    private String nombre;
+    private double precio;
+    private static int contadorProductos;
+
+}
