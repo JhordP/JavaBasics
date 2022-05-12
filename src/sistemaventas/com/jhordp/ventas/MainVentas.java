7525a02a176997fb7b81c1df36a7f5200e5f9780
JhordP <j.horda.ny@hotmail.com>
2022-05-12 09:36:15 -0400

    Sistema de ventas: Creacion clase Orden

--- src/sistemaventas/com/jhordp/ventas/MainVentas.java
+++ src/sistemaventas/com/jhordp/ventas/MainVentas.java
@@ -2,10 +2,8 @@ package sistemaventas.com.jhordp.ventas;
 
 public class MainVentas {
 
-    //Atributos de la clase
-    private int idProducto;
-    private String nombre;
-    private double precio;
-    private static int contadorProductos;
+    public static void main(String[] args) {
+        
+    }
 
 }
