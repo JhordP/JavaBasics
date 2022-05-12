edabdf9eb3c5da81885e27daac29d29ee74fbe94
JhordP <j.horda.ny@hotmail.com>
2022-05-12 13:41:00 -0400

    Sistema de Ventas: Agregado metodo agregarProducto

--- src/sistemaventas/com/jhordp/ventas/dominio/Orden.java
+++ src/sistemaventas/com/jhordp/ventas/dominio/Orden.java
@@ -14,4 +14,20 @@ public class Orden {
         this.productos = new Producto[Orden.MAX_PRODUCTOS];
     }
 
+    //Metodos
+    public void agregarProducto(Producto producto) {
+
+        //Si la cantidad de productos del objeto(Orden) es menor que el maximo de productos
+        //Entonces, para que no de un OutOfBoundsException, se continua el proceso.
+        if(this.contadorProductos < Orden.MAX_PRODUCTOS) {
+
+            //Se agrega el producto(objeto) en posicion del arreglo y luego se aumenta la variable.
+            productos[contadorProductos++] = producto;
+        }
+        else {
+            System.out.println("Se ha superado el limite de productos por orden.");
+            System.out.println("Existen en la compra "+contadorProductos+" productos de "+MAX_PRODUCTOS+" permitidos.");
+        }
+    }
+
 }
